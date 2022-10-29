eff85dd612d8b57487d740c2714deac477ca2c2b
OnePartHarmony <bakingharmony@gmail.com>
2022-10-29 07:44:28 -0400

    set up files for models and routes with basic depandencies, registered routes

---
+++ app/models/comment.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema(
+
+)
+
+module.exports = commentSchema
---
+++ app/models/company.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const companySchema = new mongoose.Schema(
+
+)
+
+module.exports = mongoose.model('Company', companySchema)
