e38f7226a4e7f5680f42616da3227b77f9fc1d32
OnePartHarmony <107879442+OnePartHarmony@users.noreply.github.com>
2022-10-31 14:31:22 -0400

    Update company_routes.js
    Deleted old stuff I accidentally left in at last merge

--- app/routes/company_routes.js
+++ app/routes/company_routes.js
@@ -106,20 +106,3 @@ router.delete('/companies/:id', requireToken, (req, res, next) => {
 })
 
 module.exports = router
-=======
-const express = require('express')
-const passport = require('passport')
-
-const Company = require('../models/company')
-
-const customErrors = require('../../lib/custom_errors')
-const requireOwnership = customErrors.requireOwnership
-const removeBlanks = require('../../lib/remove_blank_fields')
-const requireToken = passport.authenticate('bearer', { session: false })
-
-const router = express.Router()
-
-
-
-module.exports = router
-
